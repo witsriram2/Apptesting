d5260c7fbd284a682f825ab6d7e75fb0dce0bc5e
witsriram2 <good.mamu@gmail.com>
2016-01-16 12:18:57 +0530

    Update before-install.bat

--- before-install.bat
+++ before-install.bat
@@ -1,3 +1,2 @@
-REM Install Internet Information Server (IIS). 
-c:\Windows\Sysnative\WindowsPowerShell\v1.0\powershell.exe -Command Import-Module -Name ServerManager
-c:\Windows\Sysnative\WindowsPowerShell\v1.0\powershell.exe -Command Install-WindowsFeature Web-Server
+cd C:\inetpub\wwwroot\AppTest\
+del index.html
